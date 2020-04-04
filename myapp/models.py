994a3eae150af3a125262fea3becf8919a895a3b
hahazia <2019lihua@gmail.com>
2020-04-04 16:10:05 -0500

    load user data

--- myapp/models.py
+++ myapp/models.py
@@ -45,8 +45,11 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
-    email = models.EmailField(max_length=150)
+    email = models.EmailField(max_length=150,blank=True)
     bio = models.TextField()
+    city = models.CharField(max_length=100, blank=True)
+    date_joined = models.DateField(blank=True,null=True)
+
 
     def __str__(self):
         return self.user.username
